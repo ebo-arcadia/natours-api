6eb5b843c2f7d284960f3d3fa7f2257491538fca
Ebo Arcadia <yiboli.coding@gmail.com>
2023-10-23 10:34:30 -0400

    better file structure

--- routes/userRouters.js
+++ routes/userRouters.js
@@ -1,39 +1,16 @@
 const express = require('express');
+const userController = require('./../controllers/userController');
 
 const router = express.Router();
 
-const getAllUsers = (request, response) => {
-  response.status(200).json({
-    status: 'get all users success',
-    data: null,
-  });
-};
-const createUser = (request, response) => {
-  response.status(200).json({
-    status: 'create a user success',
-    data: null,
-  });
-};
-const getUser = (request, response) => {
-  response.status(200).json({
-    status: 'get a user success',
-    data: null,
-  });
-};
-const updateUser = (request, response) => {
-  response.status(200).json({
-    status: 'update a user success',
-    data: null,
-  });
-};
-const deleteUser = (request, response) => {
-  response.status(200).json({
-    status: 'delete a user success',
-    data: null,
-  });
-};
-
-router.route('/').get(getAllUsers).post(createUser);
-router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
+router
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
+router
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
 
 module.exports = router;
