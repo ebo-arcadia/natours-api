a0a8e899337d9725fbf9e1458c28e68260a306aa
Ebo Arcadia <yiboli.coding@gmail.com>
2023-11-13 11:09:40 -0500

    config mongdo database

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((connection) => {
+    console.info(connection.connections);
+    console.info('DB connected successfully');
+  });
 
 const app = require('./app');
 
