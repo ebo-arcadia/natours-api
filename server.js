56e8e23c799f22961df70a6802553584366967b8
Ebo Arcadia <yiboli.coding@gmail.com>
2023-11-16 10:53:56 -0500

    tour schema using mongoose

--- server.js
+++ server.js
@@ -20,6 +20,24 @@ mongoose
 
 const app = require('./app');
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 1.0,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.info(`server is listening on ${port}`);
