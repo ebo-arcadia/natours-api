66099fec764f1e3e0e8c11b85c57f784ebff37fe
Ebo Arcadia <yiboli.coding@gmail.com>
2023-12-16 14:11:24 -0500

    refactor, modulize tour api

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tourModel');
+const TourAPI = require('../utilities/tourAPI');
 
 // get top 5 tour middleware
 exports.aliasTopTours = (req, res, next) => {
@@ -10,48 +11,13 @@ exports.aliasTopTours = (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
   try {
-    // filtering query
-    const queryObj = { ...req.query };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach((element) => delete queryObj[element]);
+    const tourAPIInstance = new TourAPI(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .pagination();
+    const tours = await tourAPIInstance.query;
 
-    // advanced filtering query
-    const queryStr = JSON.stringify(queryObj).replace(
-      /\b(gte|gt|lte|lt)\b/g,
-      (match) => `$${match}`,
-    );
-
-    let query = Tour.find(JSON.parse(queryStr));
-
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-duration');
-    }
-
-    // fields limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
-    // pagination
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.page * 1 || 100;
-    const skip = (page - 1) * limit;
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) {
-        throw new Error('this page does not exist');
-      }
-    }
-
-    const tours = await query;
     res.status(200).json({
       status: 'success',
       requestedAt: req.requestedAt,
