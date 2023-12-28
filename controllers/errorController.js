d73631f09f74a3d7b2bf66780469f9a62b0bf804
Ebo Arcadia <yiboli.coding@gmail.com>
2023-12-28 12:17:57 -0500

    handle prod errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,35 @@
-module.exports = (err, req, res, next) => {
-  console.info(err.stack);
-
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
+const sendErrDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
+    error: err,
     message: err.message,
+    stack: err.stack,
   });
+};
+
+const sendErrProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error('Error!😾', err);
+    res.status(500).json({
+      status: 'error',
+      message: 'unexpected error',
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  let error;
+  error.statusCode = err.statusCode || 500;
+  error.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrProd(err, res);
+  }
 };
