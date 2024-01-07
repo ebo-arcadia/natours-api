d3bdd4ceee8d318fb400b0bab9c934f5af071b8e
Ebo Arcadia <yiboli.coding@gmail.com>
2024-01-07 14:17:15 -0500

    handle uncaught exceptions, unhandled server side errors

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+// handle unexpected caught errors
+process.on(`uncaughtException`, (uncaughtException) => {
+  console.error(uncaughtException.name, uncaughtException.message);
+  console.error(
+    'unexpected exception caught occurred!👁️ shutting down server...',
+  );
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -19,6 +28,16 @@ mongoose
   });
 
 const port = process.env.PORT || 3001;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.info(`server is listening on ${port}`);
+});
+
+process.on('unhandledRejection', (unhandledServerError) => {
+  console.error(unhandledServerError.name, unhandledServerError.message);
+  console.error(
+    'Unhandled server rejection occurred!👁️ shutting down server...',
+  );
+  server.close(() => {
+    process.exit(1);
+  });
 });
