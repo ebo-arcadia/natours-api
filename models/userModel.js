bdf5e1d7ad3348ab1d3f0b942908dfab99a550f0
Ebo Arcadia <yiboli.coding@gmail.com>
2024-01-08 15:24:34 -0500

    add user model

---
+++ models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'user name is required!'],
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'email is required!'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'invalid email'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'password is required!'],
+    min: [8, 'password must be least 8 characters'],
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'confirm email'],
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
