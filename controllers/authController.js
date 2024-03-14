130542c6ad3b9098e7d0dad5cf2cf398d7fdf4a9
Ebo Arcadia <yiboli.coding@gmail.com>
2024-03-14 15:33:53 -0400

    update password func

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,17 +12,20 @@ const getJwtToken = (id) => {
   });
 };
 
-exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
-
-  const jwtToken = getJwtToken(newUser._id);
-  res.status(201).json({
+const createSendToken = (user, statusCode, res) => {
+  const jwtToken = getJwtToken(user._id);
+  res.status(statusCode).json({
     status: 'user sign up success',
     jwtToken,
     data: {
-      user: newUser,
+      user: user,
     },
   });
+};
+
+exports.signup = catchAsync(async (req, res, next) => {
+  const newUser = await User.create(req.body);
+  createSendToken(newUser, 201, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -38,11 +41,7 @@ exports.login = catchAsync(async (req, res, next) => {
     return next(new AppError('incorrect emails or password'));
   }
 
-  const token = getJwtToken(user._id);
-  res.status(200).json({
-    status: 'success',
-    token,
-  });
+  createSendToken(user, 200, res);
 });
 
 exports.protect = catchAsync(async (req, res, next) => {
@@ -145,9 +144,17 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
 
   await user.save();
 
-  const token = getJwtToken(user._id);
-  res.status(200).json({
-    status: 'success',
-    token,
-  });
+  createSendToken(user, 200, res);
+});
+
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select('password');
+  if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
+    return next(new AppError('Your current password is incorrect', 401));
+  }
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save();
+
+  createSendToken(user, 201, res);
 });
